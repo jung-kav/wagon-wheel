9a81988d2b6060e2992c0f2f58fa7be882983e6b
Kaveer Kapoor <kaveerkapoor93@gmail.com>
2021-06-06 19:46:14 -0700

    fix: add GA tracking ID

--- config/site.js
+++ config/site.js
@@ -3,7 +3,7 @@ module.exports = {
   keywords: ["gatsby", "gatsbyjs", "i18n", "eslint"],
   description: "Wagon Wheel Public Website",
   author: "Kaveer Kapoor",
-  trackingId: "",
+  trackingId: "G-112LM9D669",
   defaultLang: "en",
   langTextMap: {
     en: "English",
